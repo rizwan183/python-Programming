5f9fe7ffae855cbbb3dc78c93482e87d4832c779
rizwan183 <43751305+rizwan183@users.noreply.github.com>
2019-02-14 22:09:52 +0530

    Create fullname.py

---
+++ fullname.py
@@ -0,0 +1,7 @@
+#Q WAP to accept student firstname
+#Accept last name
+#And Display the Student Full name
+first_name=input("ENTER STUDENT FIRST_NAME ")
+last_name =input("ENTER STUDENT LAST_NAME ")
+full_name=first_name+" "+last_name
+print("STUDENT FULL NAME IS ! " +full_name)
