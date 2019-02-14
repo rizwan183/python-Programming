a18d6218a4f0b32dfb5f4032a93102854b8ad75f
rizwan183 <43751305+rizwan183@users.noreply.github.com>
2019-02-14 22:15:06 +0530

    Create avgMarks.py

---
+++ avgMarks.py
@@ -0,0 +1,13 @@
+#Q WAP to accept 
+#student no
+#student name
+#Mark1,mark2,mark3
+#form command prompt
+#calculate total mark
+#agerage mark
+student_no   = input("enter student number ")
+student_name = input("enter student name   ")
+mark1=input(" Enter mark1 ")
+mark2=input(" enter mark2 ")
+mark3=input(" enter mark3 ")
+print(f"Student_No is {student_no} student_name is {student_name} He scored total mark {int(mark1)+int(mark2)+int(mark3)} And avrg is {(int(mark1)+int(mark2)+int(mark3)/3)}")
