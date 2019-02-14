b571b1c878261f7921f2dad869c18af7ed0e5f2d
rizwan183 <43751305+rizwan183@users.noreply.github.com>
2019-02-14 22:03:03 +0530

    Create mathematics_operations.py
    In this program For operation 1 addition 2 subtract  3 multiplication 4 division using two variables

---
+++ mathematics_operations.py
@@ -0,0 +1,17 @@
+#WAP to accept two number and perform
+#ADDITION
+#SUBTRACTION
+#Multiplication
+#Division
+a=input("ENTER VALUE OF a ")
+b=input("enter value Of b ")
+print(f"YOU ENTER A= {a} AND B= {b} ") 
+print("Addition Of A+B")
+print(int(a)+int(b))
+print("subtraction Of A-B")
+print(int(a)-int(b))
+print("Multiplication Of A*B")
+print(int(a)*int(b))
+print("Division Of A/B")
+print(int(a)/int(b))
+
