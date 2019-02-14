8f9f57bfdc722e6e4e9350a7d0dbc4488f639725
rizwan183 <43751305+rizwan183@users.noreply.github.com>
2019-02-14 22:16:36 +0530

    Create GrossSalary.py

---
+++ GrossSalary.py
@@ -0,0 +1,8 @@
+#WAP TO input Emp_id ,Name,Basic_salary,HRA,DA and find out the gross salary
+Emp_id=int(input("ENTER Emp_id :"))
+Emp_name=input("Enter Emp_name :")
+Basic_salary=int(input("Enter your basic salary"))
+HRA=int(input("Enter your HRA"))
+DA=int(input("enter your DA"))
+Gross_Salary=Basic_salary+HRA+DA
+print("your Gross_Salary  : ",Gross_Salary)
